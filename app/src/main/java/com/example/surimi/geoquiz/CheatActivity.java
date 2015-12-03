45deadce9e112a036a6854f430c48269784d2baa
aanderson2323 <aanderson2323@gmail.com>
2015-12-02 21:30:08 -0600

    API level and target SDK

--- app/src/main/java/com/example/surimi/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/surimi/geoquiz/CheatActivity.java
@@ -1,11 +1,16 @@
 package com.example.surimi.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -18,6 +23,7 @@ public class CheatActivity extends AppCompatActivity {
     private boolean mIsCheater=false;
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    private TextView mShowAPITextView;
     public static Intent newIntent(Context packageContext, boolean answerIsTrue){
         Intent i = new Intent(packageContext, CheatActivity.class);
         i.putExtra(EXTRA_ANSWER_IS_TRUE,answerIsTrue);
@@ -31,6 +37,10 @@ public class CheatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        mShowAPITextView = (TextView) findViewById(R.id.APIlevelText);
+        mShowAPITextView.setText(String.format("API level %s.", Build.VERSION.SDK_INT));
+        mShowAPITextView.setTypeface(Typeface.MONOSPACE);
+        mShowAPITextView.setTextSize(12);
         if (savedInstanceState != null){
             mIsCheater = savedInstanceState.getBoolean("cheatIndex", false);
         }
@@ -49,6 +59,26 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 setAnswerShownResult(true);
                 mIsCheater=true;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+
+                    });
+                    anim.start();
+                }else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
     }
